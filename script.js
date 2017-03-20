07651aaa009262e1dded19dfbfd2a5431b1af964
Rafat <refatc16@yahoo.com>
2017-03-20 14:15:28 +0000

    rectangle function

--- script.js
+++ script.js
@@ -12,7 +12,45 @@ right(90);
 forward(50);
 }
 
-// Type your function call below
 
+function triangle (){ 
+    forward(75);
+    right(120);
+    forward(75);
+    right(120);
+    forward(75);
+    right(120);
+}
+
+function rectangle(){
+    forward(80);
+    right(90);
+    forward(80);
+    right(90);
+    forward(80);
+    right(90);
+}
+rectangle();
+function house();
+forward(50);
+right(60);
+forward(50);
+right(60);
+}
+house();
+//triangle();
+// Type your function call below
+//square();
 
 stamp();
+
+
+
+
+
+
+
+
+
+
+
